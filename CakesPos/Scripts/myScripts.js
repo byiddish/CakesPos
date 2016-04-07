d532932b686691694b192f129882267628a641df
Suchi <byiddish@gmail.com>
2016-04-06 23:41:40 -0400

    Add append to DOM function and modifications

--- CakesPos/Scripts/myScripts.js
+++ CakesPos/Scripts/myScripts.js
@@ -9,23 +9,27 @@
 
     $(".categorybtn").on('click', function () {
         var c = $(this).data("category");
-        
-        //$("#cakesDiv").replaceWith()
-        //$.post("GetProductsByCategory", { categoryId: c}, function (result) {
-
-        //    $.each(result, function(i, item) {
-        //        alert(Product[i].ProductName);
-        //    })
-        //})
-        //var category = parseInt(c);
         $.post("/home/GetProductsByCategory", { categoryId: c }, function (products) {
             $(".productbtn").remove();
             products.forEach(function (product) {
                 $("#productsInnerDiv").append("<button class=" + '"btn productbtn"' + "><img src=" + "/Uploads/" + product.Image + "></button>");
-                
+
             });
-            //$("#productDiv").append('<h1>Toby is incredible!!!!!!</h1>');
-            //$("#productsDiv").show();
         })
+    })
+
+    $(".productbtn").on('click', function () {
+        var p = $(this);
+        var id = p.data("id");
+        var productName = p.data("content");
+        var inStock = p.data("inStock");
+        var price = p.data("price");
+        var row = "<tr><td><button class=" + '"btn btn-danger delete"' + ">X</button></td><td>" + productName + "</td><td><input class=" + '"input input-sm"' + " type=" + '"number"' + " value=" + '"1"' + " /></td><td>" + price + "</td></tr>";
+        $("#orderTable").append(row);
+    })
+
+    $(".delete").on('click', function () {
+        var r = $(this);
+        r.parent().parent().remove();
     })
 })
