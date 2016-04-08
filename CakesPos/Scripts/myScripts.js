4a3a91229140ce8a9b30ca94c6309e7b64e7f2ce
Suchi <byiddish@gmail.com>
2016-04-08 17:56:15 -0400

    Add Vue.js and other modifications

--- CakesPos/Scripts/myScripts.js
+++ CakesPos/Scripts/myScripts.js
@@ -14,7 +14,7 @@
             products.forEach(function (product) {
                 var productName = product.ProductName.toString();
                 //$("#productsInnerDiv").append("<button class=" + '"btn productbtn"' + "><img src=" + "/Uploads/" + product.Image + "></button>");
-                $("#productsInnerDiv").append("<button class=" + '"btn productbtn"' + "data-id=" + product.Id + " data-content=" + '"'+productName+'"' + " data-price=" + product.Price + " data-inStock=" + product.InStock + "><img src=" + "/Uploads/" + product.Image + "></button>")
+                $("#productsInnerDiv").append("<button class=" + '"btn productbtn"' + "data-id=" + product.Id + " data-content=" + '"' + productName + '"' + " data-price=" + product.Price + " data-inStock=" + product.InStock + "><img src=" + "/Uploads/" + product.Image + "></button>")
             });
         })
     })
@@ -24,8 +24,8 @@
         var id = p.data("id");
         var productName = p.data("content");
         var inStock = p.data("inStock");
-        var price = p.data("price").toFixed(2);
-        var row = "<tr><td><button class=" + '"btn btn-danger delete"' + ">X</button></td><td>" + productName + "</td><td><input class=" + '"input input-sm"' + " type=" + '"number"' + " value=" + '"1"' + " /></td><td>$" + price + "</td></tr>";
+        var price = p.data("price");
+        var row = "<tr><td><button class=" + '"btn btn-danger delete"' + ">X</button></td><td>" + productName + "</td><td><input class=" + '"input input-sm"' + " type=" + '"number"' + " value=" + '"1"' +"min="+'"1"'+" /></td><td>$" + price + "</td></tr>";
         $("#orderTable").append(row);
     })
 
@@ -33,4 +33,19 @@
         var i = $(this).closest('tr').index();
         $("tr").eq(i).remove();
     })
+})
+
+var vm = new Vue({
+    el: '#leftdiv',
+    data: {
+        q: 3,
+        p: 65
+    },
+    computed: {
+        // a computed getter
+        t: function () {
+            // `this` points to the vm instance
+            return this.q * this.p
+        }
+    }
 })
