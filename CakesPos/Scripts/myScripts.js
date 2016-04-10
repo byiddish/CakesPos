60bbba9711529529b54a9de669674980e7dae5c0
Suchi <byiddish@gmail.com>
2016-04-10 16:39:45 -0400

    Add functionality to discount, total items and add refresh button.

--- CakesPos/Scripts/myScripts.js
+++ CakesPos/Scripts/myScripts.js
@@ -27,7 +27,9 @@
         var row = "<tr data-price=" + price + "><td><button class=" + '"btn btn-danger delete"' + ">X</button></td><td>" + productName + "</td><td><input class=" + '"input input-sm q"' + "v-model=" + '"quantity"' + " type=" + '"number"' + " value=" + '"1"' + "min=" + '"1"' + " /></td><td class=" + '"price"' + ">$" + price + "</td></tr>";
         $("#orderTable").append(row);
         var total = 0;
+        var itemCount = 0;
         $('#orderTable').find('tr').not(':first').each(function () {
+            itemCount++;
             var quantity = $(this).find('input.q').val();
             var price = $(this).data('price')
             if (quantity === undefined) {
@@ -36,12 +38,19 @@
             var t = (parseFloat(quantity) * parseFloat(price));
             total += t;
             $(this).find('.price').text(t);
+            $('#totalItems').text("Total items: " + itemCount);
         });
         if (total === NaN) {
             $('#total').text("Total: $" + 0);
         }
         else {
-            $('#total').text("Total: $" + total);
+            if (getDiscount() < 1) {
+                var d = total * getDiscount();
+                $('#total').text("Total: $" + (total - d));
+            }
+            else {
+                $('#total').text("Total: $" + (total - getDiscount()));
+            }
         }
     })
 
@@ -49,7 +58,9 @@
         var i = $(this).closest('tr').index();
         $("tr").eq(i).remove();
         var total = 0;
+        var itemCount = 0;
         $('#orderTable').find('tr').not(':first').each(function () {
+            itemCount++;
             var quantity = $(this).find('input.q').val();
             var price = $(this).data('price')
             if (quantity === undefined) {
@@ -58,21 +69,30 @@
             var t = (parseFloat(quantity) * parseFloat(price));
             total += t;
             $(this).find('.price').text(t);
+            $('#totalItems').text("Total items: " + itemCount);
         });
         if (total === NaN) {
             $('#total').text("Total: $" + 0);
         }
         else {
-            $('#total').text("Total: $" + total);
+            if (getDiscount() < 1) {
+                var d = total * getDiscount();
+                $('#total').text("Total: $" + (total - d));
+            }
+            else {
+                $('#total').text("Total: $" + (total - getDiscount()));
+            }
         }
     })
 
     $("#orderTable").on('input', function () {
         var total = 0;
+        var itemCount = 0;
         $('#orderTable').find('tr').not(':first').each(function () {
+            itemCount++;
             var quantity = $(this).find('input.q').val();
             if (quantity === "")
-            { quantity = 1;}
+            { quantity = 1; }
             var price = $(this).data('price')
             if (quantity === undefined) {
                 quantity = 1;
@@ -81,12 +101,59 @@
             var t = (parseFloat(quantity) * parseFloat(price));
             total += t;
             $(this).find('.price').text(t);
+            $('#totalItems').text("Total items: " + itemCount);
         });
         if (total === NaN) {
             $('#total').text("Total: $" + 0);
         }
         else {
-            $('#total').text("Total: $" + total);
+            if (getDiscount() < 1) {
+                var d = total * getDiscount();
+                $('#total').text("Total: $" + (total - d));
+            }
+            else {
+                $('#total').text("Total: $" + (total - getDiscount()));
+            }
         }
     });
+
+    $('#refresh').on('click', function () {
+        var total = 0;
+        var itemCount = 0;
+        $('#orderTable').find('tr').not(':first').each(function () {
+            itemCount++;
+            var quantity = $(this).find('input.q').val();
+            var price = $(this).data('price')
+            if (quantity === undefined) {
+                quantity === 0;
+            }
+            var t = (parseFloat(quantity) * parseFloat(price));
+            total += t;
+            $(this).find('.price').text(t);
+        });
+        $('#totalItems').text("Total items: " + itemCount);
+        if (total === NaN) {
+            $('#total').text("Total: $" + 0);
+        }
+        else {
+            if (getDiscount() < 1) {
+                var d = total * getDiscount();
+                $('#total').text("Total: $" + (total - d));
+            }
+            else {
+                $('#total').text("Total: $" + (total - getDiscount()));
+            }
+        }
+    });
+
+    function getDiscount() {
+        var discount = $("#totalDivInner").find('.discount').val();
+        if (discount === "") {
+            return 0;
+        }
+        else {
+            return (parseFloat(discount));
+        }
+    }
+
 })
