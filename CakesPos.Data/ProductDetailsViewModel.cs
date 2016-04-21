0723ab06d6ecd8630afcc793bccc1347229ad7d9
Suchi <byiddish@gmail.com>
2016-04-21 15:24:42 -0400

    Modify OrderDetails Modal

---
+++ CakesPos.Data/ProductDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakesPos.Data
+{
+    public class ProductDetailsViewModel
+    {
+        public int orderDetailId { get; set; }
+        public int orderId { get; set; }
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public decimal price { get; set; }
+        public int inStock { get; set; }
+
+    }
+}
