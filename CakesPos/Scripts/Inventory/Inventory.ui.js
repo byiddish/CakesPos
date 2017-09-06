b5454e0cd4f22694ecf0d6313546ebd3625eee4e
Suchi <byiddish@gmail.com>
2017-09-05 21:46:25 -0400

    Some change

--- CakesPos/Scripts/Inventory/Inventory.ui.js
+++ CakesPos/Scripts/Inventory/Inventory.ui.js
@@ -3,7 +3,7 @@
     var controller = new ScrollMagic.Controller();
 
     // create a scene
-    new ScrollMagic.Scene({ triggerElement: ".categoryNav", triggerHook: 0, duration: 5000 })
+    new ScrollMagic.Scene({ triggerElement: ".categoryNav", triggerHook: 0, duration:7000})
         .setPin(".categoryNav", { pushFollowers: false })
         .addTo(controller);
 });
